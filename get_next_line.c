e00b50bc124144524d850aeb5e0785cc000e1da9
Hans Hazairi <hanshazairi@gmail.com>
2021-05-20 02:09:27 +0800

    feat(get_next_line): add error handling

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: hbaddrul <hbaddrul@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/18 16:46:56 by hbaddrul          #+#    #+#             */
-/*   Updated: 2021/05/18 16:47:56 by hbaddrul         ###   ########.fr       */
+/*   Updated: 2021/05/20 01:33:46 by hbaddrul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,17 @@
 
 int	get_next_line(int fd, char **line)
 {
+	int		size;
+	char	*buf;
+
+	buf = malloc(BUFFER_SIZE + 1);
+	if (!buf)
+		return (-1);
+	if (fd == -1 || !line || BUFFER_SIZE < 1 || read(fd, buf, 0) == -1)
+		return (-1);
+	size = read(fd, buf, BUFFER_SIZE);
+	while (size > 0)
+	{
+	}
 	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,15 @@
 /*   By: hbaddrul <hbaddrul@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/18 16:48:08 by hbaddrul          #+#    #+#             */
-/*   Updated: 2021/05/18 16:49:12 by hbaddrul         ###   ########.fr       */
+/*   Updated: 2021/05/20 00:44:35 by hbaddrul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+# include <libft.h>
+# include <unistd.h>
 
 int	get_next_line(int fd, char **line);
 
